f28b00b2eb14c761a7ab86694f4c44e895dfcc62
SimoneCorona <corona.simone93@gmail.com>
2022-05-05 16:05:51 +0200

    scaffholding

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+vue.config.devtools = true;
+
+const app = new Vue (
+    {
+        el: '#root',
+        data: {
+                saluta : 'Ciriciao!',
+                arale: 'https://tse3.mm.bing.net/th?id=OIP.tYpdY-hR79upFSvWz0aiVgHaEK&pid=Api&P=0&w=260&h=146',
+        }
+    }
+);
